bd253a77b941405d9ad170761543714208c019a1
JeonHyungJin <qpdjgjsl@gmail.com>
2018-09-28 20:06:36 +0900

    rebuild

--- main.js
+++ main.js
@@ -1,56 +1,90 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDcoGvza-ObsnUqKmJUDQ_NWoBwi7sPRA8",
-    authDomain: "cnu-hop.firebaseapp.com",
-    databaseURL: "https://cnu-hop.firebaseio.com",
-    projectId: "cnu-hop",
-    storageBucket: "cnu-hop.appspot.com",
-    messagingSenderId: "41991803234"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
 
-  function signUp() {
-  		    var id = $("#su_id").val();
-  		    var pw = $("#su_pw").val();
-  		    var cf = $("#su_cf").val();
+var config = {
+  apiKey: "AIzaSyDcoGvza-ObsnUqKmJUDQ_NWoBwi7sPRA8",
+  authDomain: "cnu-hop.firebaseapp.com",
+  databaseURL: "https://cnu-hop.firebaseio.com",
+  projectId: "cnu-hop",
+  storageBucket: "cnu-hop.appspot.com",
+  messagingSenderId: "41991803234"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
 
-  		    if(pw != cf) {
-  		        alert("Password does not match the confirm password.");
-  		        return;
-  		    }
+function signUp() {
+  var id = $("#su_id").val();
+  var pw = $("#su_pw").val();
+  var cf = $("#su_cf").val();
 
-  		    firebase.auth().createUserWithEmailAndPassword(id, pw)
-  		            .then(function() {
-  		                alert("회원 가입 완료!");
-  		                location.href="./main.html";
-  		            })
-  		            .catch(function(e) {
-  		                $("#error #errmsg").html(e.message);
-  		                $("#error").show();
-  		                return;
-  		            });
+  if (pw != cf) {
+    alert("Password does not match the confirm password.");
+    return;
+  }
+
+  firebase.auth().createUserWithEmailAndPassword(id, pw)
+    .then(function() {
+      alert("회원 가입 완료!");
+      location.href = "./main.html";
+    })
+    .catch(function(e) {
+      $("#error #errmsg").html(e.message);
+      $("#error").show();
+      return;
+    });
 }
 
-  	//로그인 (sing in) 하는 함수
-  	  function signIn() {
-  	      var id = $("#si_id").val();
-  	      var pw = $("#si_pw").val();
-  	      firebase.auth().signInWithEmailAndPassword(id, pw)
-  	              .then(function() {
-  	                  $("#signIn").hide();
-  	                  $("#authorized").show();
-                      location.href="./main.html";
-  	              })
-  	              .catch(function(e) {
-  	                  lastWork = "signIn";
-  	                  $("#error #errmsg").html(e.message);
-  	                  $("#error").show();
-  	                  $("#signIn").hide();
-  	                  return;
-  	              });
-  	 }
+firebase.auth().onAuthStateChanged(function(user) {
 
-     function back() {
-     	      $("#" + lastWork).show();
-     	      $("#error").hide();
-     	  }
+    if (user) {
+      $(".logindata").css("display" , "none");
+      $("#login").css("display" , "none");
+      $("#login_id").html(user.email+"님 환영 합니다.");
+      $("#login_id").css("display" , "inline");
+
+    }
+    else{
+        $("#logout").css("display" , "none");
+        $("#login_id").css("display" , "none");
+
+         console.log("로그인이 필요합니다.");
+    }
+  });
+
+//로그인 (sing in) 하는 함수
+function signIn() {
+  var id = $("#si_id").val();
+  var pw = $("#si_pw").val();
+  firebase.auth().signInWithEmailAndPassword(id, pw)
+    .then(function() {
+      alert("로그인 완료!");
+      $("#signIn").hide();
+      $("#authorized").show();
+      location.href = "./main.html";
+    })
+    .catch(function(e) {
+      alert("로그인 실패!");
+      lastWork = "signIn";
+      $("#error #errmsg").html(e.message);
+      $("#error").show();
+      return;
+    });
+}
+
+function back() {
+  $("#" + lastWork).show();
+  $("#error").hide();
+}
+
+function signOut() {
+  if (!confirm("Do you really want to log out?")) {
+    return;
+  }
+
+  firebase.auth().signOut().then(function() {
+    console.log("로그 아웃!");
+    location.reload();
+  }, function(e) {
+    lastWork = "authorized";
+    $("#error").show();
+  });
+}
