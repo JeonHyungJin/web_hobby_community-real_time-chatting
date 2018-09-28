7cd6b668072ae6f98f4db5f748e672ffd61353ec
JeonHyungJin <qpdjgjsl@gmail.com>
2018-09-28 17:09:10 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.5.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDcoGvza-ObsnUqKmJUDQ_NWoBwi7sPRA8",
+    authDomain: "cnu-hop.firebaseapp.com",
+    databaseURL: "https://cnu-hop.firebaseio.com",
+    projectId: "cnu-hop",
+    storageBucket: "cnu-hop.appspot.com",
+    messagingSenderId: "41991803234"
+  };
+  firebase.initializeApp(config);
+</script>
